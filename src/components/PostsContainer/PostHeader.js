f52bb3bc496960e8dd40337eb6e69a6eda626e6b
Kim Ly Buck <kim.3993@gmail.com>
2020-02-27 22:42:35 -0600

    find and replace " with '

--- src/components/PostsContainer/PostHeader.js
+++ src/components/PostsContainer/PostHeader.js
@@ -1,15 +1,15 @@
 // You do not need to change any code in this file
 
-import React from "react";
-import "./Posts.css";
+import React from 'react';
+import './Posts.css';
 
 const PostHeader = props => {
   return (
-    <div className="post-header">
-      <div className="post-thumb-wrapper">
+    <div className='post-header'>
+      <div className='post-thumb-wrapper'>
         <img
-          alt="post header"
-          className="post-thumb"
+          alt='post header'
+          className='post-thumb'
           src={props.thumbnailUrl}
         />
       </div>
